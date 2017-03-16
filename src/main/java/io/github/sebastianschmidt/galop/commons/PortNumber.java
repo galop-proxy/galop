d6c75a09796e8a11ac8f7d63dca67cdfd087ea7a
Sebastian Schmidt <email@sebastian-schmidt.info>
2017-03-16 16:37:55 +0100

    PortNumber implemented.

---
+++ src/main/java/io/github/sebastianschmidt/galop/commons/PortNumber.java
@@ -0,0 +1,48 @@
+package io.github.sebastianschmidt.galop.commons;
+
+public final class PortNumber {
+
+    public static boolean isValidPortNumber(final int value) {
+        return 0 <= value && value <= 65535;
+    }
+
+    public static PortNumber parsePortNumber(final String s) {
+
+        final int value;
+
+        try {
+            value = Integer.parseInt(s);
+        } catch (final NumberFormatException ex) {
+            return null;
+        }
+
+        if (isValidPortNumber(value)) {
+            return new PortNumber(value);
+        } else {
+            return null;
+        }
+
+    }
+
+    private final int value;
+
+    public PortNumber(final int value) {
+
+        if (!isValidPortNumber(value)) {
+            throw new IllegalArgumentException("Port number out of range: " + value);
+        }
+
+        this.value = value;
+
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return Integer.toString(value);
+    }
+
+}
