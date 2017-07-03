23580f198e39462eec46e50cc9eca4b8265c3d19
Sebastian Schmidt <email@sebastian-schmidt.info>
2017-07-03 12:49:20 +0200

    #18 Simplified HttpModule.

--- src/main/java/io/github/galop_proxy/galop/http/HttpModule.java
+++ src/main/java/io/github/galop_proxy/galop/http/HttpModule.java
@@ -7,19 +7,30 @@ public final class HttpModule extends AbstractModule {
 
     @Override
     protected void configure() {
+        bindHandler();
+        bindParsers();
+        bindWriters();
+        bindOld();
+    }
 
+    private void bindHandler() {
         bind(ExchangeHandler.class).to(ExchangeHandlerImpl.class).in(Singleton.class);
+    }
 
+    private void bindParsers() {
         bind(MessageParser.class).to(MessageParserImpl.class).in(Singleton.class);
         bind(StartLineParser.class).to(StartLineParserImpl.class).in(Singleton.class);
         bind(HeaderParser.class).to(HeaderParserImpl.class).in(Singleton.class);
+    }
 
+    private void bindWriters() {
         bind(MessageWriter.class).to(MessageWriterImpl.class).in(Singleton.class);
         bind(StartLineWriter.class).to(StartLineWriterImpl.class).in(Singleton.class);
         bind(HeaderWriter.class).to(HeaderWriterImpl.class).in(Singleton.class);
+    }
 
+    private void bindOld() {
         bind(HttpHeaderParser.class).to(HttpHeaderParserImpl.class).in(Singleton.class);
-
     }
 
 }
