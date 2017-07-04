80e3e14fc37749968d641a7c6cd167845ee2d5ae
Sebastian Schmidt <email@sebastian-schmidt.info>
2017-07-04 15:30:41 +0200

    #18 Minor cleanups.

--- src/main/java/io/github/galop_proxy/galop/http/Constants.java
+++ src/main/java/io/github/galop_proxy/galop/http/Constants.java
@@ -11,13 +11,10 @@ final class Constants {
 
     static final Charset HEADER_CHARSET = Charset.forName("ASCII");
 
-    static final String HEADER_HOST_PREFIX = "host:";
     static final String HEADER_DATE_PREFIX = "date:";
-    static final String HEADER_SERVER_PREFIX = "server:";
     static final String HEADER_CONTENT_LENGTH_PREFIX = "content-length:";
     static final String HEADER_CONTENT_TYPE_PREFIX = "content-type:";
     static final String HEADER_CONTENT_TYPE_CHARSET_PREFIX = "charset=";
-    static final String HEADER_TRANSFER_ENCODING_PREFIX = "transfer-encoding:";
 
     static final String TRANSFER_ENCODING_IDENTITY = "identity";
     static final String TRANSFER_ENCODING_CHUNKED = "chunked";
