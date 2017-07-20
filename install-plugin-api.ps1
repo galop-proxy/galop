6985194ebd26da71a34933b30b9f7b75850116e5
Sebastian Schmidt <SebastianSchmidt@users.noreply.github.com>
2017-07-20 19:12:48 +0200

    Script to install the Plugin API in the local maven repository.

--- install-plugin-api.ps1
+++ install-plugin-api.ps1
@@ -5,7 +5,7 @@ $tempDir = [System.Guid]::NewGuid().ToString()
 New-Item -Type Directory -Name $tempDir
 Set-Location $tempDir
 
-git clone https://github.com/galop-proxy/plugin-api.git
+git clone https://github.com/galop-proxy/plugin-api.git 2> $null
 Set-Location plugin-api
 
 mvn clean install
